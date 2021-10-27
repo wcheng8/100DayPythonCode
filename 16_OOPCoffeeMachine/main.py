0e92637e0d427db2caa155bb47d4d7b434f00f6e
Wilkins Cheng <wilkinscheng@MacBook-Pro-3.hub>
2021-10-27 22:35:59 +1100

    Day16 convert coffeemachine to OOP

---
+++ 16_OOPCoffeeMachine/main.py
@@ -0,0 +1,45 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# Instantiate objects
+menu = Menu()
+coffee_bank = MoneyMachine()
+coffee_machine = CoffeeMaker()
+enough_resource = True
+
+while enough_resource:
+    # TODO 1: Print report
+    order = input(f"What would you like? ({menu.get_items()}): ")
+
+    # Check if the drink is in the menu
+    drink = menu.find_drink(order)
+
+    # Rename constants
+    if drink:
+        water = drink.ingredients['water']
+        milk = drink.ingredients['water']
+        coffee = drink.ingredients['water']
+        cost = drink.cost
+
+    if order == 'report':
+        coffee_machine.report()
+
+    if order == 'off':
+        break
+
+    # TODO 2: Check resources sufficient?
+
+    if not coffee_machine.is_resource_sufficient(drink):
+        break
+
+    # TODO 3: Process Coins
+    # TODO 4: Check transaction successful?
+
+    coffee_bank.make_payment(cost)
+
+    # TODO 5: Make Coffee.
+
+    coffee_machine.make_coffee(drink)
+
+    coffee_machine.report()
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 100 Days of Code
 
 ## This project attempts to code 100+ projects continuously for 100 Days. It's aim is to familiarize myself with python code. This follows the 100Days of Python course on Udemy. The index number on the file indicates the code for that day.
+
+15. Coffee Machine application
+16. Coffee Machine application with OOP 
