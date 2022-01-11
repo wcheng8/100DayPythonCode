a2673e9540e5e900d3759ba0401c1513337b9006
Wilkins Cheng <wilkinscheng@MacBook-Pro-7.hub>
2022-01-11 15:13:22 +1100

    auto tinder swiper

--- 50_TinderSwipe/main.py
+++ 50_TinderSwipe/main.py
@@ -2,6 +2,7 @@ import os
 
 import time
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
@@ -21,9 +22,11 @@ login_btn.click()
 
 time.sleep(1)
 
+# Facebook login
 fb_login = driver.find_element(By.XPATH, '//*[@id="s-1866641101"]/div/div/div[1]/div/div[3]/span/div[2]/button')
 fb_login.click()
 
+main_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
 print(fb_login_window)
 
@@ -32,10 +35,45 @@ print(driver.title)
 
 email = driver.find_element(By.ID, 'email')
 password = driver.find_element(By.ID, 'pass')
-fb_login_btn = driver.find_element(By.XPATH, '//*[@id="u_0_0_Bk"]')
-
+fb_login_btn = driver.find_element(By.ID, 'loginbutton')
 email.send_keys(TINDER_USER)
 password.send_keys(TINDER_PASS)
 fb_login_btn.click()
 
-## Start here
+#Allow popups
+time.sleep(2)
+
+driver.switch_to.window(main_window)
+print(driver.title)
+
+allow_btn = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/div/div[3]/button[1]/span')
+allow_btn.click()
+
+time.sleep(1)
+enable_btn = driver.find_element(By.XPATH, '//*[@id="s-1866641101"]/div/div/div/div/div[3]/button[1]/span')
+accept_btn = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[2]/div/div/div[1]/button')
+
+enable_btn.click()
+accept_btn.click()
+
+# Swipe
+time.sleep(8)
+
+for i in range(100):
+    time.sleep(1)
+
+    try:
+        like_btn = driver.find_element(By.XPATH, '//*[@id="s-138260025"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]/button/span/span')
+        like_btn.click()
+
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element(By.CSS_SELECTOR, '.itsAMatch a')
+            match_popup.click()
+
+        except NoSuchElementException:
+            time.sleep(2)
+
+    except NoSuchElementException:
+        time.sleep(1)
+
